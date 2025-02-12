1c213c53bc3c7079b3ec9a528bf69211bf501fd8
Nikola <nikoladjunisic@gmail.com>
2025-02-12 21:01:00 +0100

    Added Application project classes

---
+++ BuzzUp_API.Application/DTO/TablesSearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzUp_API.Application.DTO
+{
+    public class TablesSearch : PagedSearch
+    {
+        public string Keyword { get; set; }
+    }
+}
