1040bc0cdabbdaa1f5f4fa4fc1ebe17efdc05f51
Nikola <nikoladjunisic@gmail.com>
2025-02-12 21:07:04 +0100

    Added implementation project classes

---
+++ BuzzUp_API.Implementation/UseCases/EfUseCase.cs
@@ -0,0 +1,24 @@
+﻿using BuzzUp_API.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzUp_API.Implementation.UseCases
+{
+    public abstract class EfUseCase
+    {
+        private readonly BuzzUpContext _context;
+
+
+        protected EfUseCase(BuzzUpContext context)
+        {
+            _context = context;
+
+        }
+
+        protected BuzzUpContext Context => _context;
+
+    }
+}
---
+++ BuzzUp_API.Implementation/UseCases/EfUseCaseMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BuzzUp_API.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzUp_API.Implementation.UseCases
+{
+    public class EfUseCaseMapper : EfUseCase
+    {
+        private readonly IMapper _mapper;
+
+        public EfUseCaseMapper(BuzzUpContext context, IMapper mapper) : base(context)
+        {
+            _mapper = mapper;
+        }
+
+        protected IMapper Mapper => _mapper;
+    }
+}
