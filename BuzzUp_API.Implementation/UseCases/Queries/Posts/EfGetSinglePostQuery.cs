8eba7dcd1e2b714e2288c3f67c5f1ee8af55ff6c
Nikola-02 <nikoladjunisic@gmail.com>
2026-09-23 22:13:03 +0200

    Added single post view

--- BuzzUp_API.Implementation/UseCases/Queries/Posts/EfGetSinglePostQuery.cs
+++ BuzzUp_API.Implementation/UseCases/Queries/Posts/EfGetSinglePostQuery.cs
@@ -1,24 +1,61 @@
 using AutoMapper;
+using BuzzUp_API.Application;
 using BuzzUp_API.Application.DTO.Posts;
+using BuzzUp_API.Application.Exceptions;
 using BuzzUp_API.Application.UseCases.Queries.Posts;
 using BuzzUp_API.DataAccess;
 using BuzzUp_API.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuzzUp_API.Implementation.UseCases.Queries.Posts
 {
     public class EfGetSinglePostQuery : EfFindUseCase<PostDTO, Post>, IGetSinglePostQuery
     {
-        public EfGetSinglePostQuery(BuzzUpContext context, IMapper mapper) : base(context, mapper)
+        private readonly IApplicationActor _actor;
+
+        public EfGetSinglePostQuery(BuzzUpContext context, IMapper mapper, IApplicationActor actor) : base(context, mapper)
         {
+            _actor = actor;
         }
 
         public override int Id => 12;
 
         public override string Name => "Find Post";
+
+        public override PostDTO Execute(int id)
+        {
+            var post = Context.Posts.Find(id);
+
+            if (post == null || !post.IsActive || post.DeletedAt != null || !CanView(post))
+            {
+                throw new EntityNotFoundException(nameof(Post), id);
+            }
+
+            return base.Execute(id);
+        }
+
+        private bool CanView(Post post)
+        {
+            if (post.UserId == _actor.Id)
+            {
+                return true;
+            }
+
+            if (post.VisibilityType.Name == "Public")
+            {
+                return true;
+            }
+
+            if (post.VisibilityType.Name == "Friends")
+            {
+                return Context.Friendships.Any(f =>
+                    f.IsActive &&
+                    f.DeletedAt == null &&
+                    f.FriendRequestStatus.Name == "Accepted" &&
+                    ((f.SenderUserId == _actor.Id && f.ReceiverUserId == post.UserId) ||
+                     (f.ReceiverUserId == _actor.Id && f.SenderUserId == post.UserId)));
+            }
+
+            return false;
+        }
     }
 }
